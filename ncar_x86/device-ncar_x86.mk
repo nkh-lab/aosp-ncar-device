ee780311dc82c4352281f126ccc05ff495d6b3b8
Mykola Khyliuk <nkh@ua.fm>
2022-05-04 12:48:36 +0200

    Update vehicle-hal.mk path

--- ncar_x86/device-ncar_x86.mk
+++ ncar_x86/device-ncar_x86.mk
@@ -1,3 +1,3 @@
 $(call inherit-product-if-exists, vendor/nkh-lab/car-api-hello-world/car-api-hello-world.mk)
 $(call inherit-product-if-exists, vendor/nkh-lab/genivi-capi-someip-examples/aosp/config.mk)
-$(call inherit-product-if-exists, vendor/nkh-lab/vehicle-hal/vehicle-hal.mk)
+$(call inherit-product-if-exists, vendor/nkh-lab/interfaces/automotive/vehicle/vehicle-hal.mk)

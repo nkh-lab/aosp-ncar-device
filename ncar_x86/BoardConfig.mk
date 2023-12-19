d344052734b59fb148a052afe92aa17686621ca5
Mykola Khyliuk <nkh@ua.fm>
2023-12-19 15:47:14 +0100

    Fix partition size error for Android 13

--- ncar_x86/BoardConfig.mk
+++ ncar_x86/BoardConfig.mk
@@ -1,1 +1,9 @@
 include device/generic/car/generic_car_x86/BoardConfig.mk
+
+# Fix partition size error, set current + 1MB(1048576 = 0x100000):
+#
+# RuntimeError: sum of sizes of ['emulator_dynamic_partitions'] is greater than or equal to BOARD_SUPER_PARTITION_SIZE:
+# 8589934592 == 8589934592 >= 5856296960 == 5856296960
+#
+# 8589934592+1048576 == 8590983168
+BOARD_SUPER_PARTITION_SIZE := 8590983168
